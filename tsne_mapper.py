fc8d4dff45d824b40775092f5425f05b139aa237
Noel Rogers <noel.rogers@cremeglobal.com>
2018-08-28 14:49:02 +0100

    Add igraph to test script

--- tsne_mapper.py
+++ tsne_mapper.py
@@ -12,6 +12,9 @@ import graphviz as gz
 import itertools
 import colorsys
 import sklearn
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+import igraph as ig
 
 np.random.seed(1234)
 
@@ -50,10 +53,13 @@ graph = mapper.map(lens, X.values,
 
 # Get the players per cluster
 node_dict = {}
+node_list = []
 for node in graph['nodes']:
+    node_list.append(node)
     players = [names[i] for i in graph['nodes'][node]]
     node_dict[node] = players
 
+"""
 # Use graphviz to create a custom plot
 network = gz.Graph()
 network.attr(layout='neato')
@@ -84,3 +90,92 @@ for node in graph['nodes']:
 
 mapper.visualize(graph, path_html="premier_league_players_2.html",
                  title="Player Data", inverse_X=X.values, inverse_X_names=names, custom_tooltips=names)
+"""
+
+# Create igraph viz
+
+# Add the edges:
+edge_list = []
+for node in graph['links']:
+    for nbr in graph['links'][node]:
+        # Need to base everything on indices for igraph
+        edge_list.append((node_list.index(node), node_list.index(nbr)))
+
+n_nodes = len(node_list)
+G = ig.Graph(n_nodes)
+
+G.add_edges(edge_list)
+
+avg_points = []
+for node in G.vs.indices:
+    avg_points.append(np.average([df2.iloc[i]['Points'] for i in graph['nodes'][node_list[node]]]))
+
+links = G.get_edgelist()
+layt = G.layout('kk')
+
+N = len(layt)
+Xnodes = [layt[k][0] for k in range(N)]  # x-coordinates of nodes
+Ynodes = [layt[k][1] for k in range(N)]  # y-coordnates of nodes
+
+Xedges = []
+Yedges = []
+for e in links:
+    Xedges.extend([layt[e[0]][0], layt[e[1]][0], None])
+    Yedges.extend([layt[e[0]][1], layt[e[1]][1], None])
+
+edge_linecolor = 'rgb(200,200,200)'
+edge_linewidth = 1.5
+colorscale = 'Viridis'
+reversescale = False
+showscale = True
+
+edges_trace = dict(type='scatter',
+                   x=Xedges,
+                   y=Yedges,
+                   mode='lines',
+                   line=dict(color=edge_linecolor,
+                             width=edge_linewidth),
+                   hoverinfo='none')
+
+nodes_trace = dict(type='scatter',
+                   x=Xnodes,
+                   y=Ynodes,
+                   mode='markers',
+                   marker=dict(symbol='dot',
+                               colorscale=colorscale,
+                               showscale=showscale,
+                               reversescale=reversescale,
+                               color=avg_points,
+                               colorbar=dict(thickness=20,
+                                             ticklen=4)),
+                   text=[],
+                   hoverinfo='text')
+
+axis = dict(showline=False,
+            zeroline=False,
+            showgrid=False,
+            showticklabels=False,
+            title='')
+
+layout = dict(title='Test',
+              font=dict(size=12),
+              showlegend=False,
+              autosize=False,
+              width=700,
+              height=700,
+              xaxis=dict(axis),
+              yaxis=dict(axis),
+              hovermode='closest',
+              plot_bgcolor='rgba(20,20,20, 0.8)')
+
+# Add tooltips
+nodes = G.vs.indices
+
+for node in nodes:
+    node_name = node_list[node]
+    players = node_dict[node_name]
+    node_info = node_name + '<br>-------<br>' + '<br>'.join(players)
+    nodes_trace['text'] += tuple([node_info])
+
+plot(dict(data=[edges_trace, nodes_trace], layout=layout))
+
