8449c8d4455e8eb6354354c171c97f479a0d59ba
Noel Rogers <noel.rogers@cremeglobal.com>
2018-08-31 15:01:48 +0100

    Updates to igraph plot tooltips

--- tsne_mapper.py
+++ tsne_mapper.py
@@ -5,41 +5,84 @@
 
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib import colors, cm
 import kmapper as km
-import graphviz as gz
-import itertools
-import colorsys
+from kmapper.visuals import *
 import sklearn
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from networkx_plot import *
 
 import igraph as ig
 
+
+def get_cluster_summary(player_list, average_mean, average_std, dataset, columns):
+    # Compare players against the average and list the attributes that are above and below the average
+
+    cluster_mean = np.mean(dataset.iloc[player_list].values, axis=0)
+    diff = cluster_mean - average_mean
+    std_m = np.sqrt((cluster_mean - average_mean) ** 2) / average_std
+
+    stats = sorted(zip(columns, cluster_mean, average_mean, diff, std_m), key=lambda x: x[4], reverse=True)
+    above_stats = [a[0] + ': ' + f'{a[1]:.2f}' for a in stats if a[3] > 0]
+    below_stats = [a[0] + ': ' + f'{a[1]:.2f}' for a in stats if a[3] < 0]
+
+    # Create a string summary for the tooltips
+    cluster_summary = 'Above Mean:<br>' + '<br>'.join(above_stats[:5]) + \
+                      '<br><br>Below Mean:<br>' + '<br>'.join(below_stats[-5:])
+
+    return cluster_summary
+
+
 np.random.seed(1234)
 
-# Lod dataset
+# Load dataset
 df = pd.read_csv("DetailedPlayerStats.csv", encoding='ANSI')
 
 # Remove redundant features
 to_drop = ['Id', 'FirstName', 'SecondName', 'DreamTeam', 'SelectedBy',
            'Position', 'Team', 'FixtureWeek1', 'Opponent1', 'HomeAway1',
            'FixtureWeek2', 'Opponent2', 'HomeAway2', 'FixtureWeek3', 'Opponent3',
-           'HomeAway3', 'GameWeek', 'Cost', 'NetTransfers']
+           'HomeAway3', 'GameWeek', 'Cost', 'NetTransfers', 'GoalsConceded']
 features = [col for col in df.columns if col not in to_drop]
 df = df[features]
+
+# Filter out players who didn't play
+df = df[df['MinutesPlayed'] > 0]
+
+
+# Function to convert all stats to per 90
+def min_converter(row):
+    mins = row['MinutesPlayed']
+    ret = []
+    for col in row.index:
+        if col not in ['WebName', 'MinutesPlayed']:
+            ret.append(row[col] / mins * 90)
+        else:
+            ret.append(row[col])
+    return row
+
+
+# Convert all values to per 90 mins
+df = df.apply(min_converter, axis=1)
+
+# Drop the mins played - want to base players on their performance rather than time on the pitch
+df.drop('MinutesPlayed', axis=1, inplace=True)
+df.drop('CleanSheet', axis=1, inplace=True)
+
+# Add some derived stats
+df['PassAccuracy'] = (df['PassesCompleted'] / df['AttemptedPasses']).replace(np.nan, 0)
 
 # Get average stats per game per player
 g = df.groupby(u'WebName')
 df2 = g.aggregate(np.mean)
 
-# Filter out players who didn't play
-df2 = df2[df2['MinutesPlayed'] > 0]
-
-# Drop fantasy related clounms - we only want to cluster players based on their in-game performance
+# Drop fantasy related colunms - we only want to cluster players based on their in-game performance
 X = df2[[col for col in df2.columns if col not in ['Points', 'Bonus']]]
 names = X.index.values
 X.index = [i for i in range(X.shape[0])]
+
+# Get averages for each stat
+means = np.mean(X.values, axis=0)
+std_dev = np.std(X.values, axis=0)
 
 # Initialise mapper and create lens using TSNE
 mapper = km.KeplerMapper(verbose=0)
@@ -49,52 +92,23 @@ lens = mapper.fit_transform(X.values, projection=sklearn.manifold.TSNE(), scaler
 graph = mapper.map(lens, X.values,
                    # clusterer=sklearn.cluster.DBSCAN(eps=0.3, min_samples=1),
                    clusterer=sklearn.cluster.KMeans(n_clusters=2, random_state=1234),
-                   nr_cubes=30, overlap_perc=0.9)
+                   nr_cubes=20, overlap_perc=0.9)
 
-# Get the players per cluster
+# Get the players per cluster and overall cluster stats
 node_dict = {}
 node_list = []
+node_stats = []
 for node in graph['nodes']:
     node_list.append(node)
     players = [names[i] for i in graph['nodes'][node]]
     node_dict[node] = players
+    node_stats.append(get_cluster_summary(graph['nodes'][node], means, std_dev, X, X.columns))
 
-"""
-# Use graphviz to create a custom plot
-network = gz.Graph()
-network.attr(layout='neato')
-network.attr('edge', len='0.7')
-network.attr('node', shape='circle', fixedsize='true', width='0.3',
-             label=None, fontsize='9', style='filled', fontcolor='white')
+# mapper.visualize(graph, path_html="premier_league_players_2.html",
+#                  title="Player Data", inverse_X=X.values, inverse_X_names=names, custom_tooltips=names)
 
-for node in graph['links']:
-    for nbr in graph['links'][node]:
-        # We'll remove the 'cube' and 'cluster' strings from the label to avoid clutter
-        node = str.replace(str.replace(node, 'cube', ''), 'cluster', '')
-        nbr = str.replace(str.replace(nbr, 'cube', ''), 'cluster', '')
-        network.edge(node, nbr)
 
-# Colour the nodes based on the average x value of points in the cluster
-norm = colors.Normalize(vmin=df2['Points'].min(), vmax=df2['Points'].max())
-clrmap = cm.ScalarMappable(norm=norm, cmap=cm.viridis)
-
-for node in graph['nodes']:
-    avg_pts = np.average([df2.iloc[i]['Points'] for i in graph['nodes'][node]])
-    color = clrmap.to_rgba(avg_pts)
-    color = colorsys.rgb_to_hsv(color[0], color[1], color[2])
-    node = str.replace(str.replace(node, 'cube', ''), 'cluster', '')
-    network.node(node, fillcolor="%f, %f, %f" % (color[0], color[1], color[2]))
-
-# Display the graph
-# network.render('gz_plot.pdf', view=True)
-
-mapper.visualize(graph, path_html="premier_league_players_2.html",
-                 title="Player Data", inverse_X=X.values, inverse_X_names=names, custom_tooltips=names)
-"""
-
-# Create igraph viz
-
-# Add the edges:
+# Add the edges to a list for passing into iGraph:
 edge_list = []
 for node in graph['links']:
     for nbr in graph['links'][node]:
@@ -109,6 +123,7 @@ G.add_edges(edge_list)
 avg_points = []
 for node in G.vs.indices:
     avg_points.append(np.average([df2.iloc[i]['Points'] for i in graph['nodes'][node_list[node]]]))
+    G.vs[node]['size'] = len(node_dict[node_list[node]])
 
 links = G.get_edgelist()
 layt = G.layout('kk')
@@ -123,29 +138,29 @@ for e in links:
     Xedges.extend([layt[e[0]][0], layt[e[1]][0], None])
     Yedges.extend([layt[e[0]][1], layt[e[1]][1], None])
 
-edge_linecolor = 'rgb(200,200,200)'
-edge_linewidth = 1.5
-colorscale = 'Viridis'
-reversescale = False
-showscale = True
-
 edges_trace = dict(type='scatter',
                    x=Xedges,
                    y=Yedges,
                    mode='lines',
-                   line=dict(color=edge_linecolor,
-                             width=edge_linewidth),
+                   line=dict(color='rgb(200,200,200)',
+                             width=0.5),
                    hoverinfo='none')
 
 nodes_trace = dict(type='scatter',
                    x=Xnodes,
                    y=Ynodes,
                    mode='markers',
+                   opacity=0.8,
                    marker=dict(symbol='dot',
-                               colorscale=colorscale,
-                               showscale=showscale,
-                               reversescale=reversescale,
+                               colorscale='Viridis',
+                               showscale=True,
+                               reversescale=False,
                                color=avg_points,
+                               # size=[x+1 for x in np.log2(G.vs['size'])],
+                               size=G.vs['size'],
+                               # size=5,
+                               line=dict(color='rgb(200,200,200)',
+                                         width=0.5),
                                colorbar=dict(thickness=20,
                                              ticklen=4)),
                    text=[],
@@ -170,12 +185,16 @@ layout = dict(title='Test',
 
 # Add tooltips
 nodes = G.vs.indices
+div = '<br>-------<br>'
 
+sizes = []
+trace = []
 for node in nodes:
     node_name = node_list[node]
     players = node_dict[node_name]
-    node_info = node_name + '<br>-------<br>' + '<br>'.join(players)
+    sizes.append(len(players))
+    node_info = node_name + div + '<br>'.join(players) + div + node_stats[node]
     nodes_trace['text'] += tuple([node_info])
+    trace += tuple([node_info])
 
 plot(dict(data=[edges_trace, nodes_trace], layout=layout))
-
